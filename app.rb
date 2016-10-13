6e5e781b6aa524457b650860758113388eb59326
Lorin Werthen <werthenlorin@gmail.com>
2016-10-13 11:26:35 +0200

    initial

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'sinatra/namespace'
+
+require 'sinatra/reloader' if development?
+
+require 'json'
+
+get '/:file' do
+  path = "files/#{params[:file]}"
+  halt 404, 'File not found' unless File.exist? path
+  send_file path
+end
+
+get '/' do
+  erb :upload
+end
+
+post '/' do
+  tempfile = params[:file][:tempfile]
+  filename = params[:file][:filename]
+  ext = File.extname(filename)
+
+  newname = ''
+  path = ''
+
+  loop do
+    newname = SecureRandom.base64(6).tr('/+', '-_') + ext
+    path = "files/#{newname}"
+    break unless File.exist? path
+  end
+
+  File.open(path, 'wb') do |f|
+    f.write(tempfile.read)
+  end
+
+  newname
+end
---
+++ public/progress.js
@@ -0,0 +1,52 @@
+$(document).ready(
+    function(){
+        $('input:file').change(
+            function(){
+                if ($(this).val()) {
+                    $('.file-upload .button .icon').removeClass('hidden');
+                    $('.file-upload .button').addClass('is-disabled');
+                    $('input:submit').removeClass('is-disabled');
+                }
+            }
+        );
+
+        var random_shit_index = 0;
+        var random_shit = [
+            'a cat picture',
+            'a video of a moose',
+            'your diary',
+            'secret source code'
+        ];
+
+        setInterval(function(){
+            $('.random-shit').fadeOut();
+            $('.random-shit').promise().done(function () {
+                $('.random-shit').text(random_shit[random_shit_index++ % random_shit.length]);
+                $('.random-shit').fadeIn();
+            });
+        }, 3000);
+
+        var progress = $('progress');
+
+        $('form').ajaxForm({
+            beforeSend: function() {
+                $('progress').attr('value', 0);
+                $('form').hide();
+                $('.title').fadeOut();
+                progress.fadeIn();
+            },
+            uploadProgress: function(event, position, total, percentComplete) {
+                $('progress').text(percentComplete + '%');
+            },
+            complete: function(xhr) {
+                $('progress').attr('value', 100);
+                $('.title').promise().done(function () {
+                    $('.title').text('File uploaded');
+                    $('.title').fadeIn();
+                });
+                progress.fadeOut();
+
+                $('.subtitle').text('http://zeus.ugent.be/z.e.u.s./' + xhr.responseText);
+            }
+        });
+    });
