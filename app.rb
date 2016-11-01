0e1d5910b1daa66f058db97d9ac8967a100f3c96
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-11-01 22:04:18 +0100

    fix lol security

--- app.rb
+++ app.rb
@@ -40,8 +40,6 @@ post '/' do
   newname = ''
   path = ''
 
-  halt 500, 'Error' unless %w(.png .jpg .jpeg).include? ext
-
   loop do
     newname = SecureRandom.base64(6).tr('/+', '-_') + ext
     path = "files/#{newname}"
