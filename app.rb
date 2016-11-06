9e134a91f6abdb453b30c15898dc1c72a5c8a2d6
Lorin Werthen <werthenlorin@gmail.com>
2016-11-06 21:15:01 +0100

    minor formatting

--- app.rb
+++ app.rb
@@ -8,21 +8,23 @@ require 'json'
 helpers do
   def protected!
     return if authorized?
+
     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
     halt 401, "Not authorized\n"
   end
 
   def authorized?
-    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['ZEUSWPI_USER'], ENV['ZEUSWPI_PASS']]
+    return true unless ENV['ZEUSWPI_USER'] && ENV['ZEUSWPI_PASS']
+
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && (@auth.credentials == [ENV['ZEUSWPI_USER'], ENV['ZEUSWPI_PASS']])
   end
 end
-
 
 get '/:file' do
   path = "files/#{params[:file]}"
   halt 404, 'File not found' unless File.exist? path
-  response.header['Content-Disposition'] = "attachment"
+  response.header['Content-Disposition'] = 'attachment'
   send_file path
 end
 
