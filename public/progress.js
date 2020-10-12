b0a833e1f8e0a4824b1cdb21a4601266b1fba87f
mcbloch <maxime.bloch@ugent.be>
2020-10-12 22:28:48 +0200

    extract jquery vars, better hover color

--- public/progress.js
+++ public/progress.js
@@ -1,7 +1,7 @@
 $(document).ready(
-    function(){
+    function () {
         $('input:file').change(
-            function(){
+            function () {
                 if ($(this).val()) {
                     $('.file-upload .button .icon').removeClass('hidden');
                     $('.file-upload .button').addClass('is-disabled');
@@ -19,54 +19,60 @@ $(document).ready(
             'a copyrighted video'
         ];
 
-        setInterval(function(){
-            $('.random-shit').fadeOut();
-            $('.random-shit').promise().done(function () {
-                $('.random-shit').text('Upload ' + random_shit[random_shit_index++ % random_shit.length]);
-                $('.random-shit').fadeIn();
+        let randomShitDiv = $('.random-shit')
+
+        setInterval(function () {
+            randomShitDiv.fadeOut();
+            randomShitDiv.promise().done(function () {
+                randomShitDiv.text('Upload ' + random_shit[random_shit_index++ % random_shit.length]);
+                randomShitDiv.fadeIn();
             });
         }, 3000);
 
         var progress = $('progress');
+        let title = $('.title');
+        let form = $('form');
+        let feedback = $('.feedback');
 
-        $('form').ajaxForm({
-            beforeSend: function() {
+        form.ajaxForm({
+            beforeSend: function () {
                 $('progress').attr('value', 0);
                 $('form input').prop("disabled", true);
-                $('.title').fadeOut();
-                $('.title').promise().done(function() {
-                    $('.title').text('File uploading...');
-                    $('.title').fadeIn();
+                title.fadeOut();
+                title.promise().done(function () {
+                    title.text('File uploading...');
+                    title.fadeIn();
                 });
                 progress.fadeIn();
             },
-            uploadProgress: function(event, position, total, percentComplete) {
+            uploadProgress: function (event, position, total, percentComplete) {
                 $('progress').attr('value', percentComplete);
             },
-            complete: function(xhr) {
+            complete: function (xhr) {
                 $('progress').attr('value', 100);
                 progress.fadeOut();
             },
-            success: function(responseText, statusText, xhr, form) {
-                $('.title').promise().done(function () {
-                    $('.title').text('File uploaded');
-                    $('.title').fadeIn();
+            success: function (responseText, statusText, xhr, form) {
+                title.promise().done(function () {
+                    title.text('File uploaded');
+                    title.fadeIn();
                 });
-                $('form').fadeOut();
-                $('form').promise().done(function() {
-                    $('.feedback').text(document.location.href + responseText);
-                    $('.feedback').fadeIn();
+                form.fadeOut();
+                form.promise().done(function () {
+                    let imageUrl = document.location.href + responseText
+                    feedback.html(`<a href="${imageUrl}">${imageUrl}</a>`);
+                    feedback.fadeIn();
                 });
             },
-            error: function(xhr) {
-                $('.title').promise().done(function() {
-                    $('.title').text("Something went wrong :-(");
-                    $('.title').fadeIn();
+            error: function (xhr) {
+                title.promise().done(function () {
+                    title.text("Something went wrong :-(");
+                    title.fadeIn();
                 });
-                $('form').fadeOut();
-                $('form').promise().done(function() {
-                    $('.feedback').text("Try another file perhaps. It's not *our* fault.");
-                    $('.feedback').fadeIn();
+                form.fadeOut();
+                form.promise().done(function () {
+                    feedback.text("Try another file perhaps. It's not *our* fault.");
+                    feedback.fadeIn();
                 });
             }
         });
