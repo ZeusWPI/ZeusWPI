a5516ddd027aebbe5e84ee6161cbf9e115e17b44
Francis <francisklinck@gmail.com>
2021-09-07 21:38:13 +0200

    move meta tag

--- src/templates/layout/head/head.pl
+++ src/templates/layout/head/head.pl
@@ -5,7 +5,8 @@
 head -->
     html([
         title('ZeusWPI'),
-        \uikit
+        \uikit,
+        meta([name='viewport', content='width=device-width', 'initial-scale'='1.0'])
     ]).
 
 uikit -->
@@ -15,6 +16,5 @@ uikit -->
             rel=stylesheet
         ]),
         script([src='https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js'], []),
-        script([src='https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js'], []),
-        meta([name='viewport', content='width=device-width', 'initial-scale'='1.0'])
+        script([src='https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js'], [])
     ]).
