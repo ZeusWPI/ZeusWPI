3cf8a2b2b027501724708798a97ce838288101d7
Francis <francisklinck@gmail.com>
2021-10-31 17:42:03 +0100

    add 2 starting modes

--- src/main.pl
+++ src/main.pl
@@ -4,17 +4,40 @@
 
 :- use_module('handlers').
 
-:- initialization(server(5000)).
+:- initialization(main).
 
-server(Port) :-
+main :- 
+    (current_prolog_flag(argv, [Mode, Port]) ->
+        (atom_number(Port, PortNr) ->
+            main(Mode, PortNr)
+            ;
+            usage, halt(1)
+        )
+        ;
+        usage, halt(1)
+    ).
+main(devel, Port) :-
+    write("Mode: Development"), nl,
+    write("Port: "), write(Port), nl,
+    server(Port, (make, sleep(5))).
+main(prod, Port) :-
+    write("Mode: Production"), nl,
+    write("Port: "), write(Port), nl,
+    server(Port, sleep(100)).
+
+server(Port, After) :-
     http_set_session_options([
         timeout(86400  ), % Sessions last a day.
         cookie( session)  % Don't use the default session cookie name.
     ]),
     http_server(http_dispatch, [port(Port)]),
-    refresh.
+    busy(After).
 
-refresh :-
-    make,
-    sleep(5),
-    refresh.
+% Print usage.
+usage :- write("Usage: swipl src/main.pl [prod|devel] PORT"), nl.
+
+% Infinitly keep backtracking the After Goal.
+busy(After) :-
+    repeat,
+    call(After),
+    fail.
