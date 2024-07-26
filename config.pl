4d62e181fdf5f3e5d6af0046f4735afbde62d5d6
FKD13 <44001949+FKD13@users.noreply.github.com>
2024-07-26 15:39:25 +0200

    allow webp as an image

--- config.pl
+++ config.pl
@@ -23,6 +23,7 @@ content_type('image/gif'    , '.gif').
 content_type('image/jpeg'   , '.jpg').
 content_type('image/png'    , '.png').
 content_type('image/svg+xml', '.svg').
+content_type('image/webp'   , '.webp').
 
 content_type('application/zip', '.zip').
 content_type('application/pdf', '.pdf').
